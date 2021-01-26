a3f7e1eae2e91ed655014a264fabad2081ee47b2
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2021-01-26 17:46:17 -0500

    moved data.js to server.js and added api/products route

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Solar Panels',
+            category: 'Energy',
+            image: '/images/solarpanels.jpg',
+            price: 1000000,
+            countInStock: 150,
+            brand: 'Tesla',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality solar arrays'
+        },
+        {
+            _id: '2',
+            name: 'Desalination Plant',
+            category: 'Water',
+            image: '/images/desalinationplant.jpg',
+            price: 2000000,
+            countInStock: 0,
+            brand: 'Wata',
+            rating: 4.5,
+            numReviews: 9,
+            description: 'Purified salt water'
+        },
+        {
+            _id: '3',
+            name: 'City-Wide Wifi',
+            category: 'Telecommunications',
+            image: '/images/telecommunications.jpg',
+            price: 7000000,
+            countInStock: 5,
+            brand: 'CanYouHearMeNow',
+            rating: 4.5,
+            numReviews: 11,
+            description: '4G is fine.'
+        },
+        {
+            _id: '4',
+            name: 'High Speed Rail',
+            category: 'Transportation',
+            image: '/images/highspeedrail.jpg',
+            price: 10000000,
+            countInStock: 1,
+            brand: 'FastLane',
+            rating: 4.5,
+            numReviews: 21,
+            description: 'high quality people and product movers'
+        },
+        {
+            _id: '5',
+            name: 'Vertical Farming',
+            category: 'Agriculture',
+            image: '/images/verticalfarming.jpg',
+            price: 3000000,
+            countInStock: 25,
+            brand: 'GrowRight',
+            rating: 4.5,
+            numReviews: 3,
+            description: 'high quality food production'
+        },
+        {
+            _id: '6',
+            name: 'Mixed Used Development',
+            category: 'Housing',
+            image: '/images/housing.jpg',
+            price: 50000000,
+            countInStock: 50,
+            brand: 'Urban Development LLC',
+            rating: 4.5,
+            numReviews: 5,
+            description: 'high quality residential and commerical space'
+        },
+    ],
+};
+
+export default data;
