b48eaaa89752fe26c91489f060982ab42205eb23
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2021-01-27 00:28:49 -0500

    installed redux, react-redux and redux-thunk, created a redux store to fetched data from backend

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,12 @@ const app = express();
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
-})
+});
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
+const port = process.env.PORT || 5000;
 app.listen(5000, () => {
-    console.log('Serve at http://localhost:5000');
+    console.log(`Serve at http://localhost:${port}`);
 });
