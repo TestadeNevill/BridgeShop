702e6f60a6cfd9413db4b70d5b75d4640c0f0758
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2021-03-11 21:34:13 -0500

    most recent edits

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,12 +1,36 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+// Update store with product update actions
 
-export const productListReducer = (state = { laoding: true, products: [] }, action) => {
+const {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_FAIL,
+} = require('../constants/productConstants');
+
+export const productListReducer = (
+    state = { loading: true, products: [] },
+    action
+) => {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
             return { loading: true };
         case PRODUCT_LIST_SUCCESS:
             return { loading: false, products: action.payload };
         case PRODUCT_LIST_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+export const productDetailsReducer = (state = { product: {}, loading: true }, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true };
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload };
+        case PRODUCT_DETAILS_FAIL:
             return { loading: false, error: action.payload };
         default:
             return state;
